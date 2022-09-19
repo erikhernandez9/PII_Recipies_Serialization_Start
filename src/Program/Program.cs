8e28fcdc8d6043355e74ea13fba5b16d05095ff5
clouse9 <erikfhernandezv@gmail.com>
2022-09-19 20:54:36 -0300

    Compuertas logicas terminadas

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,19 +3,28 @@ namespace CompuertaLogica
 {
     public class Program
     {
-
-        ICalculable calculable;
-        IConcetable concetable;
-        ICompuerta compuerta;
-
         public static void Main(string[] args)
         {
+            ICalculable calculable;
+            IConcetable concetable;
+            ICompuerta compuerta;
+
             Entrada ent1 = new Entrada(1);
             Entrada ent2 = new Entrada(0);
+
             OR or1 = new OR("or-1");
+            AND and1 = new AND("and-1");
+            NOT not1 = new NOT("not-1");
+
             or1.AgregarEntrada("a",ent1);
-            or1.AgregarEntrada("a",ent2);
-            Console.WriteLine(or1.Calcular());
+            or1.AgregarEntrada("b",ent2);
+            and1.AgregarEntrada("a",ent1);
+            and1.AgregarEntrada("b",ent2);
+            not1.AgregarEntrada("b",and1);
+
+            Console.WriteLine($"Or: {or1.Calcular()}");
+            Console.WriteLine($"And: {and1.Calcular()}");
+            Console.WriteLine($"Not: {not1.Calcular()}");
         }
     }
 }
